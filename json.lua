7d59ef9b3eaad08298fce1072d267cb75f9ab83f
dafengge0913 <dafengge0913@hotmail.com>
2020-07-28 14:04:30 +0800

    Skip function when encode table

--- json.lua
+++ json.lua
@@ -90,7 +90,9 @@ local function encode_table(val, stack)
       if type(k) ~= "string" then
         error("invalid table: mixed or invalid key types")
       end
-      table.insert(res, encode(k, stack) .. ":" .. encode(v, stack))
+      if type(v) ~= "function" then
+        table.insert(res, encode(k, stack) .. ":" .. encode(v, stack))
+      end
     end
     stack[val] = nil
     return "{" .. table.concat(res, ",") .. "}"
